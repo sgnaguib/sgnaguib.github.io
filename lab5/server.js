b2da84d3dcb9810c1e2f717ac89e50918d4c2567
Saeed <sgnaguib@gmail.com>
2020-02-25 13:08:39 -0800

    deployment changes

--- lab5/server.js
+++ lab5/server.js
@@ -7,7 +7,6 @@ let app = express();
 
 
 app.use(express.static(path.join(__dirname,'public')));
-//app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
@@ -17,27 +16,21 @@ app.get('/', (req,res) => {
 })
 
 app.post('/submit',(req, res)=> {
-    //console.log(req.body)
     artist_db.add(req.body)
     res.send(artist_db.getall());
 });
 
 app.post('/remove',(req, res)=> {
     artist_db.remove(req.body)
-    //console.log(artist_db.getall());
     res.send("successfully removed!");
 });
 
 app.get('/getArtists',(req, res)=> {
-    //console.log(artist_db.getall());
     res.send(artist_db.getall());
 });
 
 app.post('/search',(req, res)=> {
-    // console.log("SEARCH: " + req.body)
     matches = artist_db.search(req.body)
-    // console.log("SERVER")
-    // console.log(matches);
     res.send(matches);
 });
 
